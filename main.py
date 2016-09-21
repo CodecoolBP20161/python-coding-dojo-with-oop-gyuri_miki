eac194b7cdab1cc198755c50cb788d5deb8307a4
Miklos Csepella <csepella.miki@gmail.com>
2016-09-21 09:46:57 +0200

    done w 3/7

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+class ContactList:
+    def __init__(self):
+
 
 class Contact:
-    pass
+    all_contacts = ContactList()
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+
+    @classmethod
+    def reset_contacts(cls):
+        for i in Contact.all_contacts:
+            Contact.all_contacts.remove(i)
+
+
+class Supplier(Contact):
+    all_orders = {}
+
+    # def __init__(self):
+    #     super().__init__(args, kwargs)
+    #     pass
+
+    def order(self, _string):
+        Supplier.all_orders[self.email] = [_string]
+
+
+
